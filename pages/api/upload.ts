f94d135aacb051beb71a5c5185b25446caaad25d
KKI147 <111959118+KKI147@users.noreply.github.com>
2023-03-22 01:00:42 +0900

    Upload API File Refactoring

--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -1,19 +1,19 @@
 import nextConnect from "next-connect";
-import { existsSync, mkdirSync } from "fs";
 import multer from "multer";
-import path from "path";
+import { existsSync, mkdirSync } from "fs";
+import { NextApiRequest, NextApiResponse } from "next";
 import dayjs from "dayjs";
+
+console.log("API 호출13");
 
 const storage: any = multer.diskStorage({
   destination: function (req, file, cb) {
     const uploadPath: string = "uploads";
-    console.log("파일 시스템");
-
+    console.log(uploadPath);
     if (!existsSync(uploadPath)) {
-      console.log(uploadPath);
+      console.log("경로에 해당 폴더 가 없을 때: " + uploadPath);
       mkdirSync(uploadPath, { recursive: true });
     }
-
     cb(null, uploadPath);
   },
   filename: function (req, file, cb) {
@@ -26,23 +26,25 @@ const upload = multer({
   storage: storage,
 });
 
-const app = nextConnect({
-  onError(error, req, res: any) {
-    res.status(501).json({
-      error: `Sorry something Happened! ${error.message}`,
-      content: error,
-    });
-  },
-  onNoMatch(req, res) {
-    res.status(405).json({ error: `Method '${req.method}' Not Allowed` });
-  },
+let uploadFile = upload.single("file");
+
+const handler = nextConnect();
+handler.use(uploadFile);
+handler.post(async (req: any, res: NextApiResponse<any>) => {
+  console.log("req.body", req.body);
+  console.log(req.file);
+  let url = "http://" + req.headers.host;
+
+  // let filename = req.file.filename;
+  // console.log(filename);
+
+  res.status(200).send({
+    ok: true,
+    // result: url + "/public/" + req.file.filename,
+  });
 });
 
-app.post(upload.single("file"), function (req, res, next) {
-  res.status(200).json({ file: req.file });
-});
-
-export default app;
+export default handler;
 
 export const config = {
   api: {
---
+++ pages/api/uploadddd.ts
@@ -0,0 +1,50 @@
+import nextConnect from "next-connect";
+import { existsSync, mkdirSync } from "fs";
+import multer from "multer";
+import path from "path";
+import dayjs from "dayjs";
+
+const storage: any = multer.diskStorage({
+  destination: function (req, file, cb) {
+    const uploadPath: string = "uploads";
+
+    if (!existsSync(uploadPath)) {
+      console.log(uploadPath);
+      mkdirSync(uploadPath, { recursive: true });
+    }
+
+    cb(null, uploadPath);
+  },
+  filename: function (req, file, cb) {
+    const nowDate = dayjs(Date.now()).format("YYMMDDHHMM");
+    cb(null, `${nowDate}_${file.originalname}`);
+  },
+});
+
+const upload = multer({
+  storage: storage,
+});
+
+const app = nextConnect({
+  onError(error, req, res: any) {
+    res.status(501).json({
+      error: `Sorry something Happened! ${error.message}`,
+      content: error,
+    });
+  },
+  onNoMatch(req, res) {
+    res.status(405).json({ error: `Method '${req.method}' Not Allowed` });
+  },
+});
+
+app.post(upload.single("file"), function (req, res, next) {
+  res.status(200).json({ file: req.file });
+});
+
+export default app;
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
